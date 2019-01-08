d569b1dc424960e692f1ba4472052245a8a165a8
elusivenode <elusivenode@gmail.com>
2019-01-08 12:31:21 +1000

    Exercise 16 chapter 1.

---
+++ src/ch1/1_16_ex.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getln(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+    int len = 0;
+    char line[MAXLINE];
+
+    while ((len = getln(line, MAXLINE)) > 0) {
+        printf("LENGTH: %d\n", len);
+        printf("LINE CONTETS: %s\n", line);
+   }
+    return 0;
+}
+
+int getln(char s[], int lim) {
+    int c,i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char to[], char from[]) {
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0'){
+        ++i;
+    }
+}
